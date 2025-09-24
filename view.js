524d259caa96cab3e05a3d3e5721c31bd21be7ce
WellDone88 <kenneth.moren@live.no>
2025-09-24 13:16:21 +0200

    h

--- view.js
+++ view.js
@@ -3,11 +3,8 @@ updateView()
 
 function updateView(){
     let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
-    <input type="text" onchange="">
-    
-    
-    
-    
+    <input type="text" onchange="ReverseWord(this.value)">
+    <div id="results"></div>
     
     
     
