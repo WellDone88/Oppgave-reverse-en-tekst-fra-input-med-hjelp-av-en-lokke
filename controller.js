17482190dbdeed743483a47c31eeed3238369820
WellDone88 <kenneth.moren@live.no>
2025-09-24 14:05:54 +0200

    comments

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
-function ReverseWord(){
-
+//Har ikke bestemt meg helt for array enda, men vi får se. Rebecca tips under.
+function ReverseWord(){   
+for (let i = array.length - 1; i >= 0; i--) {
+    console.log(array[i]);
+  
 }
 
-function ShowMeResults(){
-
+document.getElementById("results").innerHTML // bygger opp evt arrays eller variabler før = kommer
 }
